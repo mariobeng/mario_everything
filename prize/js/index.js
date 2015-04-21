60ede0eba152b70c31de949e3bed46fc24e55f23
fankai <ssaas>
2015-04-21 11:42:55 +0800

    抽奖页面删除
    唯简

--- prize/js/index.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-	var b_width = $(window).width();
-	var a_width = $("a.pri_btn").width();
-	var a_left = (b_width - a_width)/2;
-	$("a.pri_btn").css({
-		"left" : a_left,
-		"height" : a_width,
-	});
-})
