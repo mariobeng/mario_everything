e72b3cee6f219ff6faf4d5f2515f65ba144856ec
mariobeng <fankaibeng@sina.com>
2015-09-08 16:17:10 +0800

    测试-预加载
    合并js

---
+++ queryloader2/js/reload.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $("body").queryLoader2({
+        barColor: "#fff",
+        backgroundColor: "#202020",
+        percentage: true,
+        barHeight: 30,
+        completeAnimation: "fade"
+    });
+});
