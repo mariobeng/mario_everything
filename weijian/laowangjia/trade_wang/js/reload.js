bf9d14438ed788a7add23015e79328ff16d4909a
mariobeng <fankaibeng@sina.com>
2015-09-15 13:41:02 +0800

    唯简-老王家
    预加载js改到最新

--- weijian/laowangjia/trade_wang/js/reload.js
+++ weijian/laowangjia/trade_wang/js/reload.js
@@ -1,9 +1,11 @@
-$(document).ready(function () {
-    $("body").queryLoader2({
+window.addEventListener('DOMContentLoaded', function() {
+    new QueryLoader2(document.querySelector("body"), {
         barColor: "#fff",
         backgroundColor: "#202020",
         percentage: true,
         barHeight: 30,
         completeAnimation: "fade"
+        /*minimumTime: 200,
+        fadeOutTime: 1000*/
     });
 });
