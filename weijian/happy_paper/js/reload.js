2d1bd2b81645fc5330fab2d887f336d0a174b3d5
mariobeng <fankaibeng@sina.com>
2015-09-16 15:09:22 +0800

    唯简-开心纸业
    添加

---
+++ weijian/happy_paper/js/reload.js
@@ -0,0 +1,12 @@
+window.addEventListener('DOMContentLoaded', function() {
+    new QueryLoader2(document.querySelector("body"), {
+        barColor: "#fff",
+        backgroundColor: "#202020",
+        percentage: true,
+        barHeight: 30,
+        completeAnimation: "fade",
+        onComplete: function(){
+            my_animate();
+        }
+    });
+});
