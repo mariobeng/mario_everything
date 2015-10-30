d95fa68ea55c693e172c6db4cb8ff3c7a821170d
mariobeng <fankaibeng@sina.com>
2015-10-30 14:29:02 +0800

    唯简-涝河桥
    loading图标位置测试01

--- weijian/mutton_laoheqiao/js/reload.js
+++ weijian/mutton_laoheqiao/js/reload.js
@@ -1,18 +1,18 @@
-/*$(function(){
+$(function(){
     var load_w = $("div.load").width();
     var load_h = $("div.load").height();
 
-    var img_w = $("div.load img").width();
-    var img_h = $("div.load img").height();
+    var img_w = $("div.load span").width();
+    var img_h = $("div.load span").height();
 
     var img_l = (load_w - img_w)/2;
     var img_t = (load_h - img_h)/2;
 
-    $("div.load img").css({
+    $("div.load span").css({
         "left":img_l,
         "top":img_t
     });
-})*/
+})
 window.onload = function(){
     $("div.load").fadeOut();
 }
